64a140fab42ffd31002b74209ee3de2b634655a7
Sebastian Strobl <sebastian.strobl@infineon.com>
2023-01-26 12:31:08 +0100

    Add project files.

---
+++ CrashTestApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CrashTestApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppCenter.Start("42466295-5bcc-4139-9c40-81e056746615",
+                  typeof(Analytics), typeof(Crashes));
+
+        }
+    }
+}
